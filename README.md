e9c969f9161c84fda3c79ec16aa36d55f7c98988
Riccardo Ancona <riccardo.ancona@gmail.com>
2017-11-22 16:37:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,103 @@
+# Cento
+
+![](https://www.rd.com/wp-content/uploads/2016/04/06-cat-wants-to-tell-you-gift.jpg)
+
+Cento is a dependency hunter.
+
+Built with 💜 by [Riccardo Ancona](https://github.com/raasoft).
+
+## Table of Contents
+- [Philosophy](#philosophy)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [Credits](#credits)
+- [License](#license)
+
+## 🐣 Philosophy
+
+We believe programming should be fun and light, not stern and stressful. It's cool to be cute; using serious words without explaining them doesn't make for better results - if anything it scares people off. We don't want to be scary, we want to be nice and fun, and then _casually_ be the best choice around. _Real casually._
+
+## 💾 Installation
+
+### Dependencies
+
+To launch *Cento*, you must install:
+
+```bash
+sudo apt-get install python-pip
+sudo pip --upgrade pip
+sudo pip install urlgrabber
+sudo pip install patool
+sudo pip install pyunpack
+```
+
+## ▶️ Usage
+
+Just launch from the terminal:
+
+```bash
+sudo python ./cento.py
+```
+
+## ⚙ Configuration
+
+Put all the dependencies into the file `food.cento` in the project root. Example:
+
+```
+{
+    "wkhtmltopdf": {
+        "url": "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz", 
+        "md5": "https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz.md5", 
+        "download_folder": "./DependencyTemp/wkhtmltopdf", 
+        "unzip_folder": "/opt/", 
+        "environment_variable": "WKHTMLTOPDF", 
+        "environment_variable_path": "wkhtmltopdf"
+    },
+
+        "pandoc": {
+            "url": "https://github.com/jgm/pandoc/releases/download/2.0.3/pandoc-2.0.3-linux.tar.gz", 
+            "md5": "https://github.com/jgm/pandoc/releases/download/2.0.3/pandoc-2.0.3-linux.tar.gz.md5", 
+            "download_folder": "./DependencyTemp/pandoc", 
+            "unzip_folder": "/opt/", 
+            "environment_variable": "PANDOC", 
+            "environment_variable_path": "pandoc"
+    }
+}
+```
+
+Put http_proxy and https_proxy into `litter.cento` in the project root. Example:
+
+```
+{
+    "http_proxy": {
+        "url": "http://user:pass@proxy:port"
+    },
+
+    "https_proxy": {
+        "url": "http://user:pass@proxy:port"
+    }
+}
+```
+
+
+## ❤ Credits
+
+Big hugs to:
+
+* Sivi the unicorn 💛
+* Cento the cat 🐱
+
+Major dependencies:
+
+* [pip](https://pypi.python.org/pypi/pip)
+* [urlgrabber](http://urlgrabber.baseurl.org/)
+* [pyunpack](https://pypi.python.org/pypi/pyunpack)
+
+The following websites were a source of inspiration:
+
+* [conan](https://www.conan.io/)
+
+## 🎓 License
+
+[MIT](http://webpro.mit-license.org/)
---
+++ cento.py
@@ -0,0 +1,208 @@
+##############################################################################
+# 
+# @file            cento.py
+# @author          Riccardo Ancona
+# @date            20/11/2017
+# @brief           Batch file to install all the necessary tools to compile
+#
+# @details         Batch file to install all the necessary tools to compile
+##############################################################################
+
+# -- Dependencies for this script --
+
+# sudo apt-get install python-pip
+# sudo pip --proxy http://username:pass@proxy:port install --upgrade pip
+# sudo pip --proxy http://username:pass@proxy:port install urlgrabber
+# sudo pip --proxy http://username:pass@proxy:port install patool
+# sudo pip --proxy http://username:pass@proxy:port install pyunpack
+
+
+# ----------------------------------
+
+import os
+import errno
+import collections
+import urlgrabber
+import hashlib
+import tarfile
+import contextlib
+from pyunpack import Archive
+import urlgrabber
+import urlgrabber.grabber as grabber
+from urlgrabber.grabber import URLGrabber, URLGrabError, CallbackObject, \
+     URLParser
+from urlgrabber.progress import text_progress_meter
+import pyunpack
+import json
+
+## Tool Configuration
+
+BatchScriptFolder = os.getcwd()
+Dependency = collections.namedtuple('Dependency', 'name url md5 download_folder unzip_folder environment_variable environment_variable_path')
+dependencyList = []
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+proxy_json_file='./food.cento'
+try:
+    proxy_json_data=open(proxy_json_file)
+    proxy_data = json.load(proxy_json_data)
+    proxy_json_data.close()
+    os.environ['http_proxy'] = proxy_data['http_proxy']['url']
+    os.environ['https_proxy'] = proxy_data['https_proxy']['url']
+except:
+    os.environ['http_proxy'] = ""
+    os.environ['https_proxy'] = ""
+
+json_file='./litter.cento'
+json_data=open(json_file)
+data = json.load(json_data)
+json_data.close()
+
+for dep in data:
+    dependencyList.append(Dependency(name=dep,
+        url=data[dep]['url'], 
+        md5=data[dep]['md5'], 
+        download_folder=data[dep]['download_folder'], 
+        unzip_folder=data[dep]['unzip_folder'],
+        environment_variable=data[dep]['environment_variable'],
+        environment_variable_path=data[dep]['environment_variable_path']))
+
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def createMD5(fname):
+    hash_md5 = md5(fname)
+    with open(fname+".md5", "w") as f:
+        f.write(hash_md5)
+
+def untar(fname, outDir):
+     tarfile.open(downloadPath).extractall(unzipOutFolder)
+
+def untarzx(fname, outDir):
+    with contextlib.closing(lzma.LZMAFile(fname)) as xz:
+        with tarfile.open(fileobj=xz) as f:
+            f.extractall(outDir)     
+
+def deflate(fname, outDir):
+    try:
+        Archive(fname).extractall(outDir)
+    except pyunpack.PatoolError: 
+        print "        WARNING: Cannot unpack " + fname
+
+
+def UpdateDependency(dep):
+    print "Collecting " + "'" + dep.name + "'"
+
+    basename = os.path.basename(dep.url)
+    downloadFolder = dep.download_folder
+    downloadPath = downloadFolder +"/"+ basename
+    basenameMD5 = os.path.basename(dep.md5)
+    downloadMd5Path = downloadFolder +"/"+ basenameMD5
+    unzipOutFolder = dep.unzip_folder
+    unzipOutPath = ""
+
+    ### Download
+    
+    # try to understand if we can create the download folder
+    try: 
+        os.makedirs(dep.download_folder)
+        print "Created " + dep.download_folder
+    except OSError:
+        if not os.path.isdir(dep.download_folder):
+            print "    ERROR: Cannot " + dep.download_folder
+            raise
+
+    md5IsAvaiable = True
+
+    http_proxy = os.environ.get('http_proxy',"")
+    https_proxy = os.environ.get('https_proxy',"")
+
+    print http_proxy
+
+    ug = URLGrabber(progress_obj = text_progress_meter(), 
+        reget='simple', 
+        proxies={ 'http' : http_proxy, 'https' : https_proxy })
+
+    try:
+        f1 = ug.urlgrab(dep.md5, downloadPath, text="    Downloading "+dep.md5)
+        md5IsAvaiable = True
+    except:
+        print "    Cannot check if the dependency is already installed. Proceeding installing it from scratch."
+        md5IsAvaiable = False
+
+
+    if os.path.isfile(downloadMd5Path) == True and md5IsAvaiable == True:
+        print "    The dependency is already installed. Skipping."
+    else:
+
+        f2 = ug.urlgrab(dep.url, downloadPath, text="    Downloading "+dep.url)
+        print "    Saved in " + downloadPath
+
+        ### Deflate
+
+        # check if the unzip folder is specified
+        # if no unzip folder specified, just use a default one
+        if (unzipOutFolder == ""):
+            unzipOutPath = BatchScriptFolder+"/"+ basename
+
+        print "        Unpacking it in " + downloadPath
+        deflate(downloadPath, unzipOutFolder)
+
+        createMD5(downloadPath)
+        
+        ### Environment Vars
+
+        if (dep.environment_variable_path != ""):
+            os.environ[dep.environment_variable] = dep.environment_variable_path
+        else:
+            os.environ[dep.environment_variable] = unzipOutFolder
+
+        print "    Setting envar "+dep.environment_variable+" = "+ \
+        os.environ.get(dep.environment_variable, 'Not set')
+
+
+    print "Done.\n"
+
+#     echo -----------------------------------------------------
+#     echo Check and Get %toolName% from artifactory...
+#     echo -----------------------------------------------------
+#     %wgetFolder%\bin\wget.exe %artifactoryLink%.md5 --user=lol_build --password=lol_build -O %workFolder%\%toolName%.zip.md5
+#     CertUtil -hashfile %workFolder%\%toolName%.zip MD5 >test.md5
+#     echo Check tools Match expected MD5 CRC
+#     set "MD5Calc="
+#     for /F "skip=1 delims=" %%i in (test.md5) do if not defined MD5Calc set "MD5Calc=%%i"
+#     set MD5Calc=%MD5Calc: =%
+#     echo %MD5Calc%
+#     set /p MD5FromArt=<%workFolder%\%toolName%.zip.md5
+#     echo %MD5FromArt%
+#     if %MD5Calc%==%MD5FromArt% (
+#     echo %toolName% is up to date
+#     ) ELSE (
+#          echo "%toolName% - get up to date tool"
+#         RMDIR /S /Q %~1
+#         %wgetFolder%\bin\wget.exe %artifactoryLink% --user=lol_build --password=lol_build -O %workFolder%\%toolName%.zip 
+#         #move /Y %~1.zip 
+#         %unzipFolder%\bin\unzip.exe -o %workFolder%\%toolName%.zip -d %unzipOutFolder%
+#     )
+#     echo.
+
+
+print "### Cento ###\n\n"
+print "Mheow! Cento is going out to catch some dependencies.\n\n"
+
+for dep in dependencyList:
+    UpdateDependency(dep)
+    
+print "Cento goes back to sleep. Meohw!"
